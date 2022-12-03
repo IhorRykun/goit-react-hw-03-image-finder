f411eea23e6c5c5be2150e12af0243e34c7c69be
Ihor <ihorrykun@gmail.com>
2022-12-03 18:39:07 +0200

    update

---
+++ src/App/components/APi_Fetch.jsx
@@ -0,0 +1,5 @@
+
+
+export const APi_Fetch = () => {
+    
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { App } from 'App/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
