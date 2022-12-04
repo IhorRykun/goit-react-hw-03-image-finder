de12c4a28cc060d1c69b6d4c4a0e926113591b64
Ihor <ihorrykun@gmail.com>
2022-12-04 19:02:34 +0200

    update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import css from '../App/App.module.css';
 import { Button } from './components/form/button/Button';
-import { Form } from './components/form/form';
 
-export const App = () => {
-  return (
-    <div className={css.container}>
-      <input className={css.input} type="text" placeholder="name" />
-      <Button>Пошук</Button>
-    </div>
-  );
-};
+export class App extends React.Component {
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12'
+    )
+      .then(r => r.json())
+      .then(console.log);
+  }
+  render() {
+    return (
+      <div className={css.container}>
+        <input className={css.input} type="text" placeholder="name" />
+        <Button>Пошук</Button>
+      </div>
+    );
+  }
+}
--- src/App/components/API_Fetch/APi_Fetch.jsx
+++
@@ -1,7 +0,0 @@
-// import axios from 'axios';
-
-export const APIFetch = () => {
-    return (
-    <h2></h2>
-)
-};
