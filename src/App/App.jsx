54c13cfc34419a381641c3ca377a078b2a87eab7
Ihor <ihorrykun@gmail.com>
2022-12-05 20:11:59 +0200

    update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import css from '../App/App.module.css';
 import { Button } from './components/form/button/Button';
+import { ImSearch } from 'react-icons/im';
 
+const API_KAY = `30187143-4d7f5699d03729238b163605a`;
 export class App extends React.Component {
   componentDidMount() {
     fetch(
-      'https://pixabay.com/api/?q=cat&page=1&key=30187143-4d7f5699d03729238b163605a&image_type=photo&orientation=horizontal&per_page=12'
+      `https://pixabay.com/api/?q=cat&page=1&key=${API_KAY}&image_type=photo&orientation=horizontal&per_page=12`
     )
       .then(r => r.json())
       .then(console.log);
@@ -14,7 +16,10 @@ export class App extends React.Component {
     return (
       <div className={css.container}>
         <input className={css.input} type="text" placeholder="name" />
-        <Button>Пошук</Button>
+        <Button>
+          <ImSearch />
+          Пошук
+        </Button>
       </div>
     );
   }
--- src/App/components/form/button/Button.jsx
+++ src/App/components/form/button/Button.jsx
@@ -1,5 +1,6 @@
 import css from '../button/Button.module.css';
 
 export const Button = ({ children }) => {
-  return <button className={css.button}>{children}</button>;
+
+  return <button  className={css.button}>{children}</button>;
 };
